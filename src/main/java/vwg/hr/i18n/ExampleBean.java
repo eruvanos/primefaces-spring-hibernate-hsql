ea64518d2eb88522079dcc454fc78aa7aa6eb2b4
Maic Siemering <maic.siemering@volkswagen.de>
2015-06-03 14:43:11 +0200

    initial: jsf running

---
+++ src/main/java/vwg/hr/i18n/ExampleBean.java
@@ -0,0 +1,12 @@
+package vwg.hr.i18n;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ExampleBean {
+
+    @Override
+    public String toString() {
+        return "Hallo Welt";
+    }
+}
